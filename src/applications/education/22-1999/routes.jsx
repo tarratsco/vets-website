43990f0bf645c413d18d009690132e9c1d014022
cameron testerman <11036339+voidspooks@users.noreply.github.com>
2026-04-22 13:20:06 -0500

    feat(22-1999): generate src/applications/education/22-1999/routes.jsx via Optimus

---
+++ src/applications/education/22-1999/routes.jsx
@@ -0,0 +1,9 @@
+// src/applications/education/22-1999/routes.jsx
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+import App from './containers/App';
+
+const routes = <Route path="/" component={App} />;
+
+export default routes;
