b767fd867587a66bff12088d8db22ca533b93040
tarratsco <onix.tarratscalderon@aquia.io>
2026-04-23 09:57:24 -0600

    feat(10-5345): generate src/applications/health-care/10-5345/pages/index.js via Optimus

---
+++ src/applications/health-care/10-5345/pages/index.js
@@ -0,0 +1,17 @@
+/**
+ * Barrel export for all page modules
+ */
+export * from './requestorType';
+export * from './veteranIdentity';
+export * from './veteranContact';
+export * from './representativeInfo';
+export * from './representativeAuthUpload';
+export * from './vaFacility';
+export * from './recordTypes';
+export * from './sensitiveRecords';
+export * from './recordDateRange';
+export * from './purposeOfDisclosure';
+export * from './recipientInformation';
+export * from './authorizationExpiration';
+export * from './additionalInstructions';
+export * from './rightToRevoke';
